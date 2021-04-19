3bc3e59c37b8e8e7d2d3c140b8f3ec9cd370da12
Jaime Ramos Lara <j23rl07@gmail.com>
2021-04-19 14:56:55 +0200

    correccion de errores

--- src/main/java/acme/features/authenticated/task/AuthenticatedTaskRepository.java
+++ src/main/java/acme/features/authenticated/task/AuthenticatedTaskRepository.java
@@ -3,7 +3,6 @@ package acme.features.authenticated.task;
 import java.util.Collection;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import acme.entities.task.Task;
@@ -16,9 +15,8 @@ public interface AuthenticatedTaskRepository extends AbstractRepository {
     @Query("select tsk from Task tsk")
 	Collection<Task> findMany();
 
-    @Query("select tsk from Task tsk where tsk.id == id")
-   	Task findOneTaskById(
-        @Param("id") Integer id);
+    @Query("select tsk from Task tsk where tsk.id = ?1")
+   	Task findOneTaskById(int id);
 
 }
 	
