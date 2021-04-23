d949a2478d6737390202de9815665e3a01a39e43
almafe2510 <44414361+Almafe2510@users.noreply.github.com>
2021-04-23 19:44:26 +0200

    #16 A little change to the spam filter
    Co-Authored-By: Badayco <64406986+badagcrack@users.noreply.github.com>

--- src/main/java/acme/features/spamFilter/AnonymousSpamDetectorService.java
+++ src/main/java/acme/features/spamFilter/AnonymousSpamDetectorService.java
@@ -49,45 +49,66 @@ public class AnonymousSpamDetectorService implements AbstractListService<Anonymo
 		result = this.repository.findMany();
 		return result;
 	}
-	
-	public boolean detectSpam(final String text) {
-		 boolean esSpam;
-		 final List<String> spamWordsList= this.repository.findMany().stream().map(x->x.getWord()).collect(Collectors.toList());
-		 final List<String> spamWordsListWithoutSpaces = new ArrayList<>();
-		 
-		 for(int i=0; i<spamWordsList.size(); i++) {
-			 final String x= spamWordsList.get(i).replace(" ", "");
-			 spamWordsListWithoutSpaces.add(x);
-		 }
-		 
-		 final List<String> textList = new ArrayList<>(Arrays.asList(text.split(" ")));
-		 Double threshold=0.;
+	public Double simpleThreshold(final String sentence, final List<String> spamWordList) {
+		Double threshold = 0.;
+		final List<String> textList = new ArrayList<>(Arrays.asList(sentence.split(" ")));
 		 int i =0;
 		 while(i<textList.size()) {
 			 final String low = textList.get(i).toLowerCase();
 			 int z=0;
-			 while(z<spamWordsListWithoutSpaces.size()) {
-				 final String spLow= spamWordsListWithoutSpaces.get(z).toLowerCase();
+			 while(z<spamWordList.size()) {
+				 final String spLow= spamWordList.get(z).toLowerCase();
 				 if(low.equals(spLow)) {
-					 
-					 threshold += ((1./textList.size())*100);
-					 
-					 break;
-					 
-				 }else {
-					 if((!(i==textList.size()-1)) && (low+textList.get(i+1)).equals(spLow)) {
-						 threshold += ((1./textList.size())*100);
-						 break;
-					 }else {
-						 	threshold += 0.;
-					 }
+					 threshold += ((1./textList.size())*100);					 
+					 break;					 
+				 }else {					 
+					 threshold += 0.;					 
 				 }
 				 z++;
-				 
-			 }
+			}
 			 i++;
 			 
 		 }
+		 return threshold;
+	}
+	
+	public Double sentenceThreshold(final String word, final List<String> spamWordList) {
+		Double threshold = 0.;
+		final List<String> textList = new ArrayList<>(Arrays.asList(word.split(" ")));
+		int i =0;
+		while(i<spamWordList.size()) {
+			final String low = spamWordList.get(i).toLowerCase();
+			if(word.contains(low)) {
+				final List<String> wN = new ArrayList<>(Arrays.asList(low.split(" ")));
+				threshold += (((double)wN.size()/(double)textList.size())*100);					 
+				break;					 
+			}else {					 
+				threshold += 0.;					 
+			}
+			i++;
+			 
+		 }
+		 return threshold;
+	}
+	
+	public boolean detectSpam(final String text) {
+		 boolean esSpam;
+		 final List<String> spamWordsList= this.repository.findMany().stream().map(x->x.getWord()).collect(Collectors.toList());
+		 final List<String> spamWordsListWord = new ArrayList<>();
+		 final List<String> spamWordsListSentence = new ArrayList<>();
+
+		 for(int i=0; i<spamWordsList.size(); i++) {
+			 final String x= spamWordsList.get(i).trim();
+			 if(x.contains(" ")) {
+				 spamWordsListSentence.add(x);
+			 }else {
+				 spamWordsListWord.add(x);
+			 }
+		 }
+		 
+		 final Double thresholdSentence = this.sentenceThreshold(text.toLowerCase().trim(),spamWordsListSentence);
+		 final Double thresholdSimple = this.simpleThreshold(text.toLowerCase().trim(),spamWordsListWord);
+		 final Double threshold = thresholdSentence+thresholdSimple;		 
 		 
 		 if(threshold<=10.) {
 			 esSpam=false; 
