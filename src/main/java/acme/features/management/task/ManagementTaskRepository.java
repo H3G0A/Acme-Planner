2feae9883f72eead4ac1ace15d96ef93f2dc0a53
badagcrack <badarhndz@gmail.com>
2021-05-15 18:42:16 +0200

    Task-033 Tests implemented

--- src/main/java/acme/features/management/task/ManagementTaskRepository.java
+++ src/main/java/acme/features/management/task/ManagementTaskRepository.java
@@ -1,12 +1,14 @@
 package acme.features.management.task;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.entities.roles.Management;
 import acme.entities.tasks.Task;
+import acme.entities.workPlan.WorkPlan;
 import acme.framework.repositories.AbstractRepository;
 
 @Repository
@@ -21,4 +23,7 @@ public interface ManagementTaskRepository extends AbstractRepository{
 	@Query("select t from Task t where t.management.id = ?1")
 	Collection<Task> findManyByManagementId(int managerId);
 
+	@Query("select w from WorkPlan w join w.tasks t where t.id = ?1")
+    List<WorkPlan> findWorkPlansByTaskId(int id);
+	
 }
---
+++ src/test/java/acme/testing/anonymous/workplan/AnonymousWorkPlanShowTest.java
@@ -0,0 +1,73 @@
+package acme.testing.anonymous.workplan;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.AcmePlannerTest;
+
+public class AnonymousWorkPlanShowTest extends AcmePlannerTest {
+
+	// Lifecycle management ---------------------------------------------------
+	
+	// Test cases -------------------------------------------------------------
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/anonymous/workplans/show-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)	
+	public void showPositive(final int recordIndex, final String title, final String dateInitial, 
+		final String dateFinal, final String workload, final String status) {		
+		
+		super.clickOnMenu("Anonymous", "WorkPlans");
+		
+		super.checkColumnHasValue(recordIndex, 0, title);
+		
+		super.checkColumnHasValue(recordIndex, 1, dateInitial);
+		
+		super.checkColumnHasValue(recordIndex, 2, dateFinal);
+		
+		super.clickOnListingRecord(recordIndex);
+		
+		super.checkInputBoxHasValue("title", title);
+		
+		super.checkInputBoxHasValue("start", dateInitial);
+		
+		super.checkInputBoxHasValue("end", dateFinal);
+		
+		super.checkInputBoxHasValue("workload", workload);
+		
+		super.checkInputBoxHasValue("isPublic", status);
+	}
+	
+//	@ParameterizedTest
+//	@CsvFileSource(resources = "/anonymous/workplans/show-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+//	@Order(10)	
+//	public void showNegative(final int recordIndex, final String title, final String dateInitial, 
+//		final String dateFinal, final String workload, final String status) {		
+//		
+//		super.clickOnMenu("Anonymous", "WorkPlans");
+//		
+//		super.checkColumnHasValue(recordIndex, 0, title);
+//		
+//		super.checkColumnHasValue(recordIndex, 1, dateInitial);
+//		
+//		super.checkColumnHasValue(recordIndex, 2, dateFinal);
+//		
+//		super.clickOnListingRecord(recordIndex);
+//		
+//		super.checkInputBoxHasValue("title", title);
+//		
+//		super.checkInputBoxHasValue("start", dateInitial);
+//		
+//		super.checkInputBoxHasValue("end", dateFinal);
+//		
+//		super.checkInputBoxHasValue("workload", workload);
+//		
+//		super.checkInputBoxHasValue("isPublic", status);
+//	
+//	}
+	
+	// Ancillary methods ------------------------------------------------------
+
+
+}
