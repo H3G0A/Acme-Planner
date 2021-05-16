46af53051ccdc9c465c2f19fe066493ff9921363
canjorbus <candelajordano@gmail.com>
2021-05-16 13:00:57 +0200

    ManagerTaskListTest Test listado creado

--- src/test/java/acme/testing/manager/task/ManagerTaskListTest.java
+++ src/test/java/acme/testing/manager/task/ManagerTaskListTest.java
@@ -1,6 +1,43 @@
 package acme.testing.manager.task;
 
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class ManagerTaskListTest {
+import acme.testing.AcmePlannerTest;
 
+public class ManagerTaskListTest extends AcmePlannerTest {
+	// Test cases -------------------------------------------------------------
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/manager/task/list.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(2)
+	public void list(final int recordIndex, final String title, final String description, final String link, final String start,
+		final String end, final String workload,final String isPublic) {		
+		super.signIn("manager1", "manager1");
+
+		super.clickOnMenu("Manager", "Task List");
+		
+		super.checkColumnHasValue(recordIndex, 0, title);
+		
+		super.checkColumnHasValue(recordIndex, 3, start);
+		
+		super.checkColumnHasValue(recordIndex, 4, end);
+
+		super.clickOnListingRecord(recordIndex);
+		
+		super.checkInputBoxHasValue("title", title);
+		
+		super.checkInputBoxHasValue("description", description);
+
+		super.checkInputBoxHasValue("link", link);
+		
+		super.checkInputBoxHasValue("start", start);
+		
+		super.checkInputBoxHasValue("end", end);
+		
+		super.checkInputBoxHasValue("workload", workload);
+		
+		super.checkInputBoxHasValue("isPublic", isPublic);
+		}
 }
