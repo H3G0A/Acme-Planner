860909a57aaa946456d43d3e2b0bd21d88186b3d
almafe2510 <44414361+Almafe2510@users.noreply.github.com>
2021-05-20 11:48:52 +0200

    #63 More tests implemented

---
+++ src/test/java/acme/testing/administrator/userAccount/AdministratorUserAccountShow.java
@@ -0,0 +1,38 @@
+package acme.testing.administrator.userAccount;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.AcmePlannerTest;
+
+public class AdministratorUserAccountShow extends AcmePlannerTest {
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/administrator/userAccount/show.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void show(final int recordIndex, final String username ,final String name,final String surname,
+		final String email, final String role, final String status) {
+		super.signIn("administrator", "administrator");
+		
+		super.clickOnMenu("Administrator", "User accounts");
+		
+		super.clickOnListingRecord(recordIndex);
+		
+		super.checkInputBoxHasValue("username", username);
+		
+		super.checkInputBoxHasValue("identity.name", name);
+		
+		super.checkInputBoxHasValue("identity.surname", surname);
+		
+		super.checkInputBoxHasValue("identity.email", email);
+		
+		super.checkInputBoxHasValue("roleList", role);
+		
+		super.checkInputBoxHasValue("status", status);
+		
+		super.signOut();		
+		
+	}
+
+}
